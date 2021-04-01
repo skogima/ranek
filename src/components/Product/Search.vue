239bb6c6cbbf84797e1d5acdf883b107fd956d40
Gabriel <gabriel_kogima@hotmail.com>
2021-04-01 14:29:32 -0300

    add search component

---
+++ src/components/Product/Search.vue
@@ -0,0 +1,29 @@
+<template>
+  <form>
+    <input type="text" name="search" id="search" v-model="search" />
+    <input
+      type="submit"
+      id="submit"
+      value="Buscar"
+      @click.prevent="searchProducts"
+    />
+  </form>
+</template>
+
+<script>
+export default {
+  name: "ProductSearch",
+
+  data: () => ({
+    search: "",
+  }),
+
+  methods: {
+    searchProducts() {
+      this.$router.push({ query: { q: this.search } });
+    },
+  },
+};
+</script>
+
+<style scoped></style>
