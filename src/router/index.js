ef0a06389451844b1a7eb275485212f36d96db0c
Gabriel <gabriel_kogima@hotmail.com>
2021-04-01 14:30:10 -0300

    Add Home view

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,17 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 
+import Home from "@/views/Home.vue";
+
 Vue.use(VueRouter);
 
-const routes = [];
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+];
 
 const router = new VueRouter({
   mode: "history",
---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <section>
+    <h1>👍 Compre ou venda 👎</h1>
+    <product-search />
+    <product-list />
+  </section>
+</template>
+
+<script>
+import ProductList from "@/components/Product/List.vue";
+import ProductSearch from "@/components/Product/Search.vue";
+
+export default {
+  components: {
+    ProductList,
+    ProductSearch,
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+    color: #87f;
+    margin-top: 40px;
+    font-size: 2rem;
+    text-align: center;
+}
+</style>
