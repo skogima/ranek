8566cabf959e9416f91942661fac2e1b46c7a7ae
Gabriel <gabriel_kogima@hotmail.com>
2021-04-01 10:45:10 -0300

    add style and page layout to App

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,82 @@
 <template>
   <div id="app">
-    <router-view />
+    <ranek-header />
+    <main>
+      <router-view class="view" />
+    </main>
+    <ranek-footer />
   </div>
 </template>
 
 <script>
-export default {};
+import RanekHeader from "@/components/RanekHeader.vue";
+import RanekFooter from "@/components/RanekFooter.vue";
+
+export default {
+  components: {
+    RanekHeader,
+    RanekFooter,
+  },
+};
 </script>
 
-<style></style>
+<style>
+* {
+  box-sizing: border-box;
+}
+
+body,
+ul,
+li,
+h1,
+h2,
+p {
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: "Avenir", Arial, Helvetica, sans-serif;
+  color: #345;
+  background: url("./assets/pattern.svg") repeat top;
+}
+
+a {
+  color: #345;
+  text-decoration: none;
+}
+
+ul {
+  list-style: none;
+}
+
+.btn {
+  display: block;
+  padding: 10px 30px;
+  background: #87f;
+  border-radius: 4px;
+  color: #fff;
+  text-align: center;
+  font-size: 1rem;
+  box-shadow: 0 4px 8px rgba(30, 60, 90, 0.2);
+  transition: all 0.3s;
+  border: none;
+  font-family: "Avenir", Arial, Helvetica, sans-serif;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background: #65d;
+  transform: scale(1.1);
+}
+
+#app {
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+}
+
+main {
+  flex: 1;
+}
+</style>
