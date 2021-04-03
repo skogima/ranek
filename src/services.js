5e5a921c54cc7be9f0c4a02d6f4cabd5c8b31b61
Gabriel <gabriel_kogima@hotmail.com>
2021-04-03 15:06:51 -0300

    add viacep api

--- src/services.js
+++ src/services.js
@@ -3,3 +3,7 @@ import axios from "axios";
 export default axios.create({
   baseURL: "http://localhost:3000",
 });
+
+export function fetchCep(cep) {
+  return axios.get(`https://viacep.com.br/ws/${cep}/json`);
+}
