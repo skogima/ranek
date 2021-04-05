3971333efc60c35e7b7afc6690cae61ec2f1b462
Gabriel <gabriel_kogima@hotmail.com>
2021-04-05 13:53:51 -0300

    feat: user can update his data

--- src/views/User/UserEdit.vue
+++ src/views/User/UserEdit.vue
@@ -1,9 +1,41 @@
 <template>
-  <h1>Edição</h1>
+  <section>
+    <account-form>
+      <button class="btn" @click.prevent="updateUser">Atualizar dados</button>
+    </account-form>
+  </section>
 </template>
 
 <script>
-export default {};
+import api from "@/services";
+import AccountForm from "@/components/AccountForm.vue";
+
+export default {
+  name: "UserEdit",
+
+  components: {
+    AccountForm,
+  },
+
+  data: () => ({
+    displayError: false,
+  }),
+
+  methods: {
+    async updateUser() {
+      try {
+        const user = this.$store.state["user"].user;
+        
+        await api.put(`users/${user.id}`, user);
+
+        await this.$store.dispatch("user/fetchUser", user.id);
+        this.$router.push({ name: "User" });
+      } catch {
+        this.displayError = true;
+      }
+    },
+  },
+};
 </script>
 
 <style></style>
